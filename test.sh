137acbcd0ce30c5162590dad944f530c9ae97196
Steven Humphrey <steven.humphrey@bbc.co.uk>
2021-09-07 14:18:54 +0100

    Add neovim testing

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@
 docker build -f test/Dockerfile -t fugitive_gitlab .
 
 docker run --rm -v `pwd`:/test/fugitive-gitlab.vim -w '/test' fugitive_gitlab vim -Es -Nu vimrc -c 'Vader! fugitive-gitlab.vim/test/*' > /dev/null
+docker run --rm -v `pwd`:/test/fugitive-gitlab.vim -w '/test' fugitive_gitlab nvim -Es -Nu vimrc -c 'Vader! fugitive-gitlab.vim/test/*' > /dev/null
